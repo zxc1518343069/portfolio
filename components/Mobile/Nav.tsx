da3ae4b951d5f71895ea88be906b64bb057115fb
YiChen <1518343069@qq.com>
2024-10-16 03:47:51 +0000

    fix: 修复跳转失败

--- components/Mobile/Nav.tsx
+++ components/Mobile/Nav.tsx
@@ -3,13 +3,17 @@
 import { usePathname } from "next/navigation";
 import { navLinks } from "@/const/const";
 import Link from "next/link";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger, SheetClose } from "@/components/ui/sheet";
 import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
 
 function MobileNav() {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false)
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={(props) => {
+      setOpen(props)
+    }}>
       <SheetTrigger className={"flex items-center justify-center"}>
         <CiMenuFries className={"text-[32px] text-accent"} />
       </SheetTrigger>
@@ -26,14 +30,18 @@ function MobileNav() {
             <Link
               href={link.path}
               key={link.name}
+              onClick={() => {
+                setOpen(false)
+              }}
               className={`${pathname === link.path && "border--b-2 border-accent text-accent"} text-xl capitalize transition-all hover:text-accent`}
             >
               {link.name}
             </Link>
           ))}
         </nav>
+
       </SheetContent>
-    </Sheet>
+    </Sheet >
   );
 }
 export default MobileNav;
